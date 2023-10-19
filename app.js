f3f482cbd18a17510c0a3e0e0c6d9e2fbed00d2b
mac-ar <perrolocoar@gmail.com>
2023-10-19 19:47:43 -0300

    Version Original

---
+++ app.js
@@ -0,0 +1,19 @@
+//Modulos de Express
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+//Indico que Carpeta es Publica
+app.use(express.static(path.join(__dirname, './public')));
+
+//Declaro el puerto por donde escucha el servidor
+let port = 3000
+
+//Inicio Servidor
+app.listen(port, () => console.log(`Servidor Activo y escuhcando por puerto: ${port}`));
+
+//Rutas
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','home.html'))
+})
